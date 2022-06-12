57f70c19b69251754471bd56b3a850b448293e24
Qosim Qodirov <qosimqodiriy701@gmail.com>
2022-06-12 16:38:10 +0500

    init

---
+++ components/TheNavbar.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="wrap">
+    <div class="container">
+      <div class="brand">
+        <nuxt-link to="/">
+          <img src="@/assets/img/brand.svg" alt="" />
+        </nuxt-link>
+      </div>
+
+      <div class="show" ref="menu" :class="{ 'show-nav': isMenuOpen }">
+        <div class="links">
+          <nuxt-link class="link" @click.native="scrollToTop" to="/media-blog">
+            Media blog
+          </nuxt-link>
+          <nuxt-link class="link" @click.native="scrollToTop" to="/edu">
+            Ta’lim
+          </nuxt-link>
+          <nuxt-link class="link" @click.native="scrollToTop" to="/books">
+            Kutubxona</nuxt-link
+          >
+          <nuxt-link class="link" @click.native="scrollToTop" to="/media">
+            Media loyihalar
+          </nuxt-link>
+          <nuxt-link class="link" @click.native="scrollToTop" to="/online-edu">
+            Onlayn darslar</nuxt-link
+          >
+        </div>
+        <!-- <div class="search">
+          <input type="search" class="input" id="search" />
+          <label class="label" for="search"
+            ><img src="@/assets/img/search.svg" alt=""
+          /></label>
+        </div> -->
+      </div>
+
+      <div
+        class="burger"
+        :class="{ 'toggle-x': isMenuOpen }"
+        @click="isMenuOpen = !isMenuOpen"
+      >
+        <div class="stick"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar',
+
+  data() {
+    return {
+      isMenuOpen: false,
+    }
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.wrap {
+  background: var(--black);
+  padding: 22px 0;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  z-index: 99999999999999;
+}
+.input {
+  background: transparent;
+  border: none;
+  color: white;
+  transition: 0.3s;
+  position: absolute;
+  left: -150px;
+  width: 150px;
+}
+.input:focus {
+  outline: none;
+  box-shadow: none;
+  border-bottom: 1px solid white;
+}
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0;
+}
+.links,
+.brand {
+  display: flex;
+  align-items: center;
+}
+
+.link {
+  color: white;
+  margin-right: 40px;
+  font-size: var(--18px);
+  position: relative;
+}
+.link:last-child {
+  margin-right: 0;
+}
+.link:hover {
+  text-decoration: none;
+}
+.label {
+  border: none !important;
+  cursor: pointer;
+}
+.search {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+.nuxt-link-exact-active {
+  color: var(--orange);
+}
+.stick {
+  width: 30px;
+  height: 2px;
+  background: white;
+  position: relative;
+  transition: 0.3s;
+}
+.stick::after {
+  content: '';
+  position: absolute;
+  top: 10px;
+  left: 0;
+  width: 30px;
+  height: 2px;
+  background: white;
+  transition: 0.3s;
+}
+.stick::before {
+  content: '';
+  position: absolute;
+  top: -10px;
+  left: 0;
+  width: 30px;
+  height: 2px;
+  background: white;
+}
+
+.burger {
+  display: none;
+  z-index: 999999999999999;
+}
+
+@media screen and (max-width: 1024px) {
+  .show {
+    /* opacity: 0;
+    visibility: hidden; */
+    display: none !important;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+  }
+  .show-nav {
+    display: flex !important;
+    justify-content: center;
+    background: rgba(0, 0, 0, 0.808);
+    z-index: 999;
+  }
+  .links {
+    flex-direction: column;
+    gap: 1rem;
+  }
+  .link {
+    margin-right: 0;
+  }
+  .brand img {
+    width: 100px;
+  }
+  .burger {
+    display: block;
+  }
+  .search {
+    display: none;
+  }
+  .toggle-x .stick {
+    transform: rotate(45deg);
+  }
+  .toggle-x .stick::after {
+    transform: rotate(90deg);
+    top: 0;
+  }
+  .toggle-x .stick::before {
+    display: none;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Hello />
+</template>
+
+<script>
+import Hello from '../components/helloVue.vue'
+export default {
+  name: 'IndexPage',
+
+  components: {
+    Hello
+  }
+}
+</script>
