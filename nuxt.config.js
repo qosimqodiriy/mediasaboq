128635c42bb2a34f62a3d0e97beb383645e53b40
Qosimjon Qodirov <qosimqodiriy701@gmail.com>
2022-06-25 02:03:25 +0500

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -122,7 +122,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxt/image',
+    // '@nuxt/image',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
